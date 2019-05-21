238469b13a1776bfc0c1301ccb037206e9cde135
Rio Sanka <threehouse.second@gmail.com>
2019-05-21 14:41:29 +0900

    add research form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :name_confirmation, :password, :password_confirmation)
+    params.require(:user).permit(:name, :name_confirmation, :research, :password, :password_confirmation)
   end
 end
