55975bb22c963fd4de44d373769762cb5bafc552
Rio Sanka <threehouse.second@gmail.com>
2019-07-07 20:56:37 +0900

    change design toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @main = current_user.mains.build
-      @mains = Main.all.order("date DESC").page(params[:page]).per(8)
+      @mains = Main.where.not(date: Date.today).order("date DESC").page(params[:page]).per(8)
+      @today_mains = Main.where(date: Date.today)
     end
   end
 end
