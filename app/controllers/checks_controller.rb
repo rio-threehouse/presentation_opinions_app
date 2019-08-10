a894a2270747b88db043869fdd77f8d75cc93423
Rio Sanka <threehouse.second@gmail.com>
2019-08-10 13:58:22 +0900

    add ajax to comment

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -2,16 +2,16 @@ class ChecksController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    comment = Comment.find(params[:comment_id])
-    current_user.check(comment)
-    flash[:success] = 'チェックしました'
-    redirect_back(fallback_location: root_path)
+    @comment = Comment.find(params[:comment_id])
+    current_user.check(@comment)
+    # flash[:success] = 'チェックしました'
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    comment = Comment.find(params[:comment_id])
-    current_user.uncheck(comment)
-    flash[:danger] = 'チェックを取り消しました'
-    redirect_back(fallback_location: root_path)
+    @comment = Comment.find(params[:comment_id])
+    current_user.uncheck(@comment)
+    # flash[:danger] = 'チェックを取り消しました'
+    # redirect_back(fallback_location: root_path)
   end
 end
