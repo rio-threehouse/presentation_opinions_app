c0cf2a2d0842c9077e0a4a14843b019e38f5b103
Rio Sanka <threehouse.second@gmail.com>
2019-12-05 09:38:08 +0900

    変更 コメント最大文字数を225にする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :present
   belongs_to :user
 
-  validates :comment, presence: true, length: { maximum: 100 }
+  validates :comment, presence: true, length: { maximum: 225 }
   validates :tag, presence: true
 
   has_many :checks, dependent: :destroy
